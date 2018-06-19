c3dee605d8f61055518b7d2b00899b21cc097ed4
Diem Nguyen <t.nguyendp@gmail.com>
2018-06-19 15:17:29 -0400

    Initial commit

--- MonopolyDice/ViewController.swift
+++ MonopolyDice/ViewController.swift
@@ -9,11 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var myFirstDice: UIImageView!
+    @IBOutlet weak var mySecondDice: UIImageView!
+    @IBOutlet weak var myDisplayLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    @IBAction func rollDiceButton(_ sender: Any) {
+        var firstNum:Int = Int(arc4random_uniform(5)) + 1;
+        var secondNum:Int = Int(arc4random_uniform(5)) + 1;
+        var firstImg: String = "";
+        var secondImg: String = "";
+        
+        firstImg = "dice\(firstNum).jpg";
+        secondImg = "dice\(secondNum).jpg";
+        
+        myFirstDice.image = UIImage(named: firstImg);
+        mySecondDice.image = UIImage(named: secondImg);
+        
+        if (firstNum == secondNum) {
+            myDisplayLabel.textColor = UIColor.red;
+        }
+        else {
+            myDisplayLabel.textColor = UIColor.black;
+        }
+        myDisplayLabel.text = String(firstNum+secondNum);
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
